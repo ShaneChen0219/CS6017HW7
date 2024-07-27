70d22a3eb085da78fd2b1031387731e83fc79fa1
ShaneChen0219 <hsiangyuan0219@gmail.com>
2024-07-26 21:15:18 -0600

    Initial files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CS6017HW7
-The repo for CS6017 HW7
+## Using NBA player 2016-2022 dataset 
+u1103296 Phillip Tran
+u1483142 Hsiang-Yuan(Shane) Chen
---
+++ d3.js
@@ -0,0 +1,37 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+let players
+let selectedPlayer =[]
+window.onload = async () => {
+    
+    let data = await d3.csv("./all_seasons.csv");
+    let playerSet = new Set()
+    for (let player of data) {
+        playerSet.add(player.player_name)
+    }
+    players = Array.from(playerSet)
+    makePlayerList()
+
+}
+
+function makePlayerList(){
+    d3.select('#nbaplayers').selectAll('p')
+    .data(players)
+    .join(
+        enter =>{
+            enter.append('p')
+            .text((d,i)=>d)
+            .on("click", handlePlayerOnClick)
+        }
+    )
+}
+
+
+//parameter e = event
+//parameter d = data
+function handlePlayerOnClick(e,d){
+    let element = d3.select(this)
+    element.classed("selected", !element.classed("selected"))
+
+    
+
+}
